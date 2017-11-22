f0699141fff2af32738b42d579b4d0c328aaccee
tshemsedinov <timur.shemsedinov@gmail.com>
2017-11-22 22:08:21 +0200

    Add simple CLI

--- examples/example.js
+++ examples/example.js
@@ -1,21 +1,25 @@
+#!/usr/bin/env node
 'use strict';
+
 const fc = require('../lib/file-crypt');
 
-// Encrypting file
-{
+const command = (process.argv[2] || '').toLowerCase();
+const password = 'password';
+
+if (command === '-e') {
+  // Encrypting file
   const filename = 'example.txt';
-  const password = 'password';
   const extension = '.encrypt';
   const cipher = 'aes-256-cbc';
   const hash = 'sha256';
-
   fc.encryptFile(filename, password, extension, cipher, hash);
+} else if (command === '-d') {
+  // Decrypting file
+  const filename = 'example.txt.encrypt:83';
+  const password = 'password';
+  fc.decryptFile(filename, password);
+} else {
+  console.log('Usage: ');
+  console.log('  - encrypt file: ./example.js -e');
+  console.log('  - decrypt file: ./example.js -d');
 }
-
-// Decrypting file
-// {
-//   const filename = 'example.txt.encrypt:83';
-//   const password = 'password';
-
-//   fc.decryptFile(filename, password);
-// }
